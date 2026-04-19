d6a6702178b77105b6e741912bd4894426a6f898
Olesiaaaaaa <@.com>
2026-04-19 18:04:04 +0300

    fix: относительный путь для фавиконки + base для GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/yacht-spa-netlify/', // ← раскомментировали и поменяли имя
+  base: '/yacht-spa-netlify/', // ← важно: имя репо + слэши с двух сторон!
 })
